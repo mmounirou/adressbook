1101b75cca36def8e93cd21d69c3385f81c1bc12
mmounirou <personal-git@mmounirou.com>
2011-05-27 01:07:47 +0200

    fist working version of adress book

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,9 +10,34 @@ public class Application extends Controller
 
 	public static void index()
 	{
-
 		List<Contact> contacts = Contact.all().fetch();
 		render(contacts);
+	}
+
+	public static void edit(long id)
+	{
+		Contact contact = Contact.findById(id);
+		render(contact);
+	}
+
+	public static void updateContact(long id, String name, String firstName, String mail, String phone)
+	{
+		Contact contact = Contact.findById(id);
+		contact.name = name;
+		contact.firstName = firstName;
+		contact.mail = mail;
+		contact.phone = phone;
+		contact.save();
+		index();
+	}
+
+	public static void addContact(String name, String firstName, String mail, String phone)
+	{
+		Contact contact = new Contact(name, firstName, mail, phone);
+		contact.create();
+
+		// Refresh the interface
+		index();
 	}
 
 }
--- app/models/Contact.java
+++ app/models/Contact.java
@@ -11,4 +11,19 @@ public class Contact extends Model
 	public String firstName;
 	public String mail;
 	public String phone;
+
+	public Contact()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public Contact(String name, String firstName, String mail, String phone)
+	{
+		super();
+		this.name = name;
+		this.firstName = firstName;
+		this.mail = mail;
+		this.phone = phone;
+	}
+
 }
